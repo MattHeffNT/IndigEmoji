9c2cf2c3aeb1ae3dddc025199bb12f6638dea720
MattHeffNT <heffa2004@gmail.com>
2023-08-26 15:26:28 +1000

    change to IndigEmoji styles and content

--- src/components/Slides.tsx
+++ src/components/Slides.tsx
@@ -84,7 +84,8 @@ const Slides = React.memo(({ isOpen, onClose }: SlidesProps) => {
             <IonButton
               onClick={onClose}
               ref={closeButton}
-              style={{ marginTop: '3rem' }}>
+              style={{ marginTop: '3rem' }}
+            >
               <IonIcon name='close' className='modal-icon' size='large' />
             </IonButton>
           </IonButtons>
@@ -94,7 +95,8 @@ const Slides = React.memo(({ isOpen, onClose }: SlidesProps) => {
           ref={slidesRef}
           modules={[Pagination, EffectFade]}
           pagination={true}
-          onSlideChange={resetSlideScroll}>
+          onSlideChange={resetSlideScroll}
+        >
           {/* @ts-ignore */}
 
           <SwiperSlide>
@@ -102,27 +104,23 @@ const Slides = React.memo(({ isOpen, onClose }: SlidesProps) => {
               <IonRow>
                 <IonCol>
                   <IonImg
-                    src='../assets/welcome/welcomescreen1.jpg'
+                    src='/assets/welcome/start_header_no.png'
                     id='slide-img'
                   />
                 </IonCol>
                 <IonCol>
                   <div className='slide-container'>
-                    <h1 style={{ color: 'white' }}>Kaytetyemoji</h1>
+                    <h1 style={{ color: 'white' }}>Indigemoji</h1>
                     <strong>
                       <p>
-                        Emoji nyamernarte Kaytetye inenge Barrow Creek arenye
-                        inengele mpwarenhe. Nyartepe apmere Artnke, Barrow
-                        Creek. Nte apeke ahentye arrewethe kaltyarrewethe
-                        Kaytetye angkewe. Ethwene nte arrkantele ngkeyenge
-                        inengewe.
+                        Emoji nhenhe-areye arrwekele anthurre Australia-kenhe.
+                        Tyerretye Arrernte-areye itnenhe mpwareke,
                       </p>
                     </strong>
+
                     <p style={{ color: 'white' }}>
-                      The Kaytetyemoji app was made by Kaytetye people from
-                      Barrow Creek. The Barrow Creek emoji represents this app.
-                      These emojis are for you to learn Kaytetye language and
-                      send to your friends and family for fun whenever you like.
+                      Australia’s first set of Indigenous emojis made on
+                      Arrernte country in Mparntwe / Alice Springs.
                     </p>
                   </div>
                 </IonCol>
@@ -134,26 +132,29 @@ const Slides = React.memo(({ isOpen, onClose }: SlidesProps) => {
               <IonRow>
                 <IonCol>
                   <IonImg
-                    src='../assets/welcome/map_rounded.jpg'
+                    src='/assets/welcome/screen1_header.png'
                     id='slide-img'
                   />
                 </IonCol>
                 <IonCol>
                   <div className='slide-container'>
-                    <h1 style={{ color: 'white' }}>Nthakenhaye!</h1>
+                    <h1 style={{ color: 'white' }}>Werte!</h1>
                     <strong>
                       <p>
-                        Emoji nyamernarte angke Kaytetye apmere Thangkenharenge
-                        (Barrow Creek), altyerre inenge, altye inenge. Arrernte
-                        inengele arrwekele arle mpwarenhe, aynanthelke Kaytetye
-                        theye ngwarenhe.
+                        Anwerne akangentye arrantherre apetyeke Indigemoji-kenhe
+                        aretyeke. Arne-nhenhe-areye arrekantherre ileme
+                        anwerne-akerte, altyerre, arne, awelhentye-areye,
+                        angkentye anwernekenhe-uthene. Impene anthurre kwenhe
+                        arrantherre angkentye akaltye-irretyeke, angkentye
+                        anwernekenhe akwete-ante rlterrke antirrkwetyeke.
                       </p>
                     </strong>
                     <p style={{ color: 'white' }}>
-                      This sticker set represents life, culture and language on
-                      Kaytetye Country in the Barrow Creek region of Central
-                      Australia. The original Arrernte Indigemoji app inspired
-                      us to make a Kaytetye set.
+                      Welcome to the IndigEmoji app! This sticker set represents
+                      life, culture and language in our part of Central
+                      Australia, the traditional land of the Arrernte people. It
+                      is important you learn so that we hold our language
+                      strong, forever.
                     </p>
                   </div>
                 </IonCol>
@@ -165,7 +166,7 @@ const Slides = React.memo(({ isOpen, onClose }: SlidesProps) => {
               <IonRow>
                 <IonCol>
                   <IonImg
-                    src='../assets/welcome/photo_rounded.jpg'
+                    src='/assets/welcome/screen2_header.png'
                     id='slide-img'
                   />
                 </IonCol>
@@ -173,29 +174,14 @@ const Slides = React.memo(({ isOpen, onClose }: SlidesProps) => {
                   <div className='slide-container'>
                     <strong>
                       <p>
-                        Emoji nyamernarte Akalperre arenye inengele mpwarenhe
-                        apmere Mparntwe warle. Nte kaltyarrewethe aynewanthe,
-                        wele nyarte nte arratewethe. Emoji nharte mpwarenhe
-                        artnwenge inenge kwenyelepenhe kaltyarrewethe angke
-                        elperterre arntarrtyewethe intemaperte.
+                        Tyerretye anwerkenhe-arle itnhenhe-areye mpwareke.
+                        Arrernte ilyernpenye-areye help-me-ileme angkentye
+                        arratye arrerneke.
                       </p>
                     </strong>
                     <p style={{ color: 'white' }}>
-                      Kaytetyemoji was created by a Kaytetye family group in
-                      Mparntwe/Alice Springs. You can read our story on the{' '}
-                      <span
-                        style={{
-                          color: '#007bff',
-                          textDecoration: 'underline',
-                        }}
-                        onClick={handleLink}>
-                        aynanthe //about page.
-                      </span>
-                    </p>
-                    <p style={{ color: 'white' }}>
-                      We made this app for our young people to have strong
-                      language. It is important that you learn Kaytetye to keep
-                      our language strong, forever.
+                      These emoji stickers were made by hundreds of young people
+                      with senior Arrernte cultural advisers, for you to use.
                     </p>
                   </div>
                 </IonCol>
@@ -207,25 +193,25 @@ const Slides = React.memo(({ isOpen, onClose }: SlidesProps) => {
             <IonRow>
               <IonCol>
                 <IonImg
-                  src='../assets/welcome/screenshot_rounded.jpg'
+                  src='/assets/welcome/screen3_header.png'
                   id='slide-img'
                 />
               </IonCol>
               <IonCol>
                 <div className='slide-container'>
-                  <h1 style={{ color: 'white' }}>Etnyewethe aynewenhanthe!</h1>
+                  <h1 style={{ color: 'white' }}>Antherreme</h1>
                   <strong>
                     <p>
-                      Ethwewethe iterrtye kngwerewe, Artnke arratewethe, emoji
-                      arratewene ane arratewethe ‘ethwenke’. Platform rengele
-                      ilewethe.
+                      Unte apeke emoji renhe yernetyeke, app nhenhe
+                      altywere-ilaye, kele emoji unte ahentye-aneme uthemele,
+                      kele imernte antherreme-kenhe-button ulthemele.
                     </p>
                   </strong>
                   <p style={{ color: 'white' }}>
-                    If you want to send an emoji to someone else…just open this
-                    app, select the sticker you want to use and tap the share
-                    button. You can then choose which platform you want to share
-                    the sticker on.
+                    To share an emoji sticker, just open this app, select the
+                    sticker you want to use and then tap the share button. You
+                    can then choose which platform you want to share the sticker
+                    on.
                   </p>
                 </div>
               </IonCol>
@@ -236,30 +222,16 @@ const Slides = React.memo(({ isOpen, onClose }: SlidesProps) => {
             <IonRow>
               <IonCol>
                 <IonImg
-                  src='../assets/welcome/talk_rounded.jpg'
+                  src='/assets/welcome/screen5_header.png'
                   id='slide-img'
                 />
               </IonCol>
               <IonCol>
                 <div className='slide-container'>
-                  <h1 style={{ color: 'white' }}>Elpathewethe</h1>
-                  <strong>
-                    <p>
-                      Arratewethe elpathewethe angke Kaytetye angkerranengarle.
-                      Ntelke angkene Kaytetye angke kwere. Arratewethe
-                      elpathewethe angke makwerle Kaytetye angkerranengarle.
-                      Erlkwe Tommy Jangalala ampilerantyenkarle. English theye
-                      nte ritemayleme tyampe.{' '}
-                    </p>
-                  </strong>
+                  <h1 style={{ color: 'white' }}>Awaye!</h1>
                   <p style={{ color: 'white' }}>
-                    Press word to listen to the Kaytetye word that reflects the
-                    emoji. Then try saying the word!
-                  </p>
-                  <p style={{ color: 'white' }}>
-                    Press phrase to listen to a Kaytetye sentence with that word
-                    told by Kaytetye elder Tommy Jangala. You can read the
-                    English translation too.
+                    New audio! Press the play button to learn to pronouncethe
+                    Arrernte emojis!
                   </p>
                 </div>
               </IonCol>
--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -8,138 +8,93 @@ import './styles/Team.css';
 const Team: React.FC<any> = () => {
   return (
     <IonContent>
-      <div className="teams-container">
-        <h1> Aynanthe // team </h1>
+      <div className='teams-container'>
+        <h1> Anwerne-akerte / Team </h1>
+        <strong>
+          <p>
+            Arne-nhenhe-areye arrekantherre ileme anwerne-akerte, altyerre,
+            arne, awelhentye-areye, angkentye anwernekenhe-uthene. Tyerretye
+            anwerkenhe-arle itnhenhe-areye mpwareke. Arrernte ilyernpenye-areye
+            help-me-ileme angkentye arratye arrerneke.
+          </p>
+        </strong>
         <p>
-          <strong> emoji bosses: </strong>
-        </p>
-
-        <p>
-          <strong> Tommy Jangala “Walkabout” </strong> is a senior Kaytetye man. Born at Hatches
-          Creek, his father’s country is Akalperre and his mother’s country is Kwerrkepentye. He was
-          raised traditionally in the bush and worked as a stockman throughout much of his life. He
-          has been involved in Kaytetye language work for many years and features on the audio of
-          the Kaytetye Learner’s Guide and in the
-          <a href="https://www.youtube.com/watch?v=B5QVNwTOzgE"> video clip</a> of Vincent Janima’s
-          song ‘Artweye Erlkwaye’ . Tommy lives at Ankweleyelengkwe Outstation on his traditional
-          lands.
-        </p>
-
-        <p>
-          <strong> Phillip Janima’s </strong> father’s country is Akalperre (he is Tommy’s son) and
-          his mother’s country is Arnerre, a Kaytetye country north-west of Barrow Creek. He was
-          involved in the Arrkantele TV series, a children’s language learning show. Philip provided
-          the audio for the emoji app and translated Tommy’s Kaytetye. Philip spends his time in
-          both Alice Springs and Ankweleyelengkwe Outstation.
+          This sticker set was designed by hundreds of young people and senior
+          Arrernte cultural advisers during seven weeks of workshops as part of
+          the Alice Springs Public Library’s Geek in Residence program. It is
+          funded by the Northern Territory Government with support from the
+          Alice Springs Town Council.
         </p>
         <p>
-          <strong>Valentine Shaw’s </strong> father’s country is Akalperre and on her mother’s side
-          she is Anmatyerr from Arrwek. Valentine lives in Alice Springs. She has a Bachelor of
-          Education and has many years experience as a high school teacher. She currently works as a
-          history teacher at St Philip’s College. Valentine is passionate about Aboriginal languages
-          and keeping Kaytetye alive. She spends time on Country learning from her grandfather. She
-          contributed artwork for the emojis and provided audio for the app.
+          inDigiMOB supported a team of senior artists and digital mentors to
+          work with these young artists to realise their ideas. Leigh Harris and
+          ingeous studios developed this app. We also thank the generous support
+          of the Johnston Foundation.
         </p>
         <p>
-          <strong>Glorianna Moketarinja</strong> is a Western Arrernte woman from Ntaria. She is of
-          Pwerle skin group. She is married to Philip Janima. Glorianna contributed to the
-          translation of the Arrernte emojis from the Indigemoji app. She spends her time in both
-          Alice Springs and Ankweleyelengkwe Outstation.
+          Additional developers: Matthew Ngamurrari Heffernan,{' '}
+          <a href='https://lav.io/'>Sam Lavigne</a>
         </p>
         <p>
-          <strong>artists:</strong>
+          Emoji advisers: Veronica Dobson Perrurle, Kathleen Wallace Kemarre,
+          Joel Liddle Perrurle and Amelia Turner
+        </p>
+        <p>Producer: Caddie Brain</p>
+        <p>
+          Senior artists: Graham Wilfred, Dave Webb, Phillip McCormack, Emma
+          Stubbs, Wyona Palmer, Colleen Powell, Lindsey Matthews, John Lewis,
+          Benita Clements, Ricky Connick Jakamara, Dion Beasley and Elliat Rich
         </p>
         <p>
-          <strong>Kathleen Rambler’s</strong> father’s country is Akalperre (she is Tommy’s
-          daughter) and her mother’s country is Arnerre. Kathleen spent her childhood in the Barrow
-          Creek and Utopia regions. She is a well-known artist and contributed artwork for the
-          emojis and the Kaytetye website. Kathleen now lives at Ampilatwatja, her husband’s
-          country, and her art can be purchased from Artists of Ampilatwatja.
+          Featured young emoji artists: Abraham Lennie, Aisha, Alba, Ashley
+          Marshall, Astrid Keaney, Broadie, Cherina, Chris Jones, Denzel Steel,
+          Dustin, Elizabeth Coulthard, Ellis, Ernestine Mulkatana, Felicity
+          Malbunka, Ishmale, Jasmine Coulthard, Jessie, Joyanne Tilmouth,
+          Kaylene Nelson, Kelvin Campbell, Lilah, Lily Blackman, Lucas Jones,
+          Megan Noall, Mia Satour, Mikara Tilmouth, Mishka Kiessling, Mitchell,
+          Natasha Angus, Onna, Reshanna, Sebastian, Shinarah, Tarynt Stubbs,
+          Tjikiti Liddle, Vanessa and Vicprsha
         </p>
         <p>
-          <strong>April Phillips</strong> is a Wiradjuri-Scottish woman of the Galari peoples of
-          NSW. A digital artist and youth mentor, April is passionate about computer art for a new
-          world.
+          Youth team: Matt D’Penguin, Alice Goddard, Else Kennedy, Pathi Raj and
+          Lindsey Matthews
         </p>
         <p>
           {' '}
-          With <strong>Dave Wells</strong>,<strong> Valentine Shaw</strong> and{' '}
-          <strong>Rebecca Harnish</strong>{' '}
+          The use of Aboriginal Flag is courtesy of Harold Thomas and WAM
+          Clothing.{' '}
         </p>
         <p>
-          The original Arrernte emojis included in this set were developed by the Arrerntemoji art
-          team: Graham Wilfred Jnr, Emma Stubbs, Dion Beasley, Phillip McCormack, Benita Clements,
-          Colleen Powell, Lindsey Matthews, Agnes Saunders, Jessie Bethune, Dustin, Lucas Jones,
-          Lilah Shee, Ellis, Ernestine Mulkatana, Mishka Kiessling, Tarynt Stubbs, Sebastian,
-          Shinarah Woods, Aarha, Chris Jones, Kelvin Campbell, Kaylene Nelson, Ashley Marshall,
-          Grace Voller, Oliver Jones, Kyiesha Charles, Cherina, Vicprsha Scovie, Astrid Keaney,
-          Abraham Lennie, Denzel Steel, Felicity Malbunka, Broadie Orr, Shakira Ken, Natasha Angus,
-          Elizabeth Coulthard, Reshanna, Tjikiti Liddle, Harold Thomas and Elliat Rich.
+          Audio: Joel Liddle Perrurle, Veronica Dobson Perrurle, Kathleen
+          Wallace Kemarre and the Year 9 students at the Alice Springs Language
+          Centre/Centralian Middle School: Ada Watson, Johnien Furber, Tyrell
+          Martin, Shakira Richards, Lawrence Squires, Shalaylee Coombes,
+          Shonneille Craig, Jenaya Newchurch, Wendelle Miller Kruger. With
+          thanks to David Moore and Jannette McCormack.
         </p>
         <p>
-          <strong> app/developers: </strong>
-        </p>
-
-        <p>
-          <strong> Matthew Ngamurarri Heffernan </strong> is a Luritja technologist from central
-          Australia who has worked in various sectors from government, not for profits and private
-          industry. He is an alumnus of the School of Cybernetics at ANU, and Information Technology
-          at CDU and is committed to ensuring that Indigenous people are up-skilled in, and don’t
-          miss out on, technological advancements and economic development. He is passionate about
-          exploring the ways in which new technologies intersect with Indigenous culture and
-          knowledge as well as examining the ways in which culture can be expressed via emerging
-          mediums. He also sometimes just loves coding.
-        </p>
-        <p>
-          {' '}
-          With <a href="https://lav.io/">Sam Lavigne</a>,
-          <a href="https://mod.studio/"> Leigh Harris</a>,<a href="https://mod.studio/"> Mod </a>{' '}
-          and <a href="https://sol1.com.au/">Sol1</a> of the original Indigemoji team.
-        </p>
-        <p>
-          <strong> producers: </strong>
-        </p>
-        <p>
-          <a href="http://www.caddiebrain.com">Caddie Brain</a>, Myfany Turpin, Bridey Lea and Joel
-          Perrule Liddle
-        </p>
-
-        <p>
-          <strong> with thanks: </strong>
-        </p>
-        <p>
-          Regional Arts Australia, Arts NT, The Northern Territory Government, The University of
-          Sydney, the Johnston Foundation, Rachel Perkins, Sam Lavigne, Tega Brain, Timothy Chatwin,
-          Jennifer Pysden, Bao Stone catering, Watch This Space, and Australian National University
-        </p>
-        <p>
-          <em>
-            This project was made on Arrernte Country in Mparntwe. It was made possible by the
-            Australian Government's Regional Arts Fund, which supports the arts in regional and
-            remote Australia, the Northern Territory Government, with support from the University of
-            Sydney and the Johnston Foundation.
-          </em>
-        </p>
-
-        <p>
-          Full credits on the <a href="http://www.indigemoji.com.au/">Indigemoji website</a>.
+          Full credits on the{' '}
+          <a href='http://www.indigemoji.com.au/'>Indigemoji website</a>.
         </p>
         <IonGrid>
           <IonRow>
-            <IonCol className="teamImages">
-              <a href="https://www.indigemoji.com.au">
-                <IonImg src="/assets/logos/indigemojilogo.png"> </IonImg>
+            <IonCol className='teamImages'>
+              <a href='https://nt.gov.au'>
+                <IonImg src='/assets/logos/ntglogo.png'> </IonImg>
               </a>
-              <a href="https://www.batchelor.edu.au/">
-                <IonImg src="/assets/logos/CALL-logo-lge.png"> </IonImg>
+              <a href='https://www.indigimob.com.au/'>
+                <IonImg src='/assets/logos/indigimoblogo.png'> </IonImg>
               </a>
-              <a href="https://regionalarts.com.au/">
-                <IonImg src="/assets/logos/RegionalArtsLogos.jpg"> </IonImg>
+              <a href='http://www.alicesprings.nt.gov.au/services/library'>
+                <IonImg src='/assets/logos/library.jpg'> </IonImg>
               </a>
-              <a href="https://www.sydney.edu.au/">
-                <IonImg src="/assets/logos/Uni-Sydney-logo-lockup-mono-SCM.jpg"></IonImg>
+              <a href='http://www.desertpenguin.com.au/'>
+                <IonImg src='/assets/logos/penguin.png'></IonImg>
               </a>
-              <IonImg src="/assets/logos/The_Johnston_Foundation_logo.jpg"> </IonImg>
+              <a href='https://www.ingeousstudios.com/'>
+                <IonImg src='/assets/logos/ingeoussociallogo.png'></IonImg>
+              </a>
+              <IonImg src='/assets/logos/johnston_foundation.jpg'> </IonImg>
             </IonCol>
           </IonRow>
         </IonGrid>
